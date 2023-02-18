6b1a28fbf6cbca78081eec059626258c3a9ee003
Zhongmin Han <hanzm.vp@gmail.com>
2023-02-18 10:23:39 +0800

    Update simple_variables.sh

--- scripts/simple_variables.sh
+++ scripts/simple_variables.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 #pipeline for mapping EMS mutants
 #variables file
-trait=FT
+trait=HD
 #define the path to Java java-1.8.0 version
-java='~/anaconda3/bin/java'
+java='/public/home/zmhan/anaconda3/bin/java'
 
 #input files
 mut_files=fastq/${trait}*mut*
@@ -13,8 +12,16 @@ wt_files=fastq/${trait}*wt*
 #output names
 mutation=recessive #change to dominant if the mutation is dominant
 
-line=${trait}  ##if you prefer, change EMS to the name of your line.  Letters a\
-nd underscores only.
+line=${trait}  ##if you prefer, change EMS to the name of your line.  Letters and underscores only.
 mut=${line}_mut
 wt=${line}_wt
+
+
+
+my_species=Oryza_sativa_Japonica #paste your species name here to replace Arabidopsis_thaliana
+
+SNPEFF="~/software/SNPEFF/snpEff/snpEff.jar"
+annName="Japonica7.0"
+
+
 
